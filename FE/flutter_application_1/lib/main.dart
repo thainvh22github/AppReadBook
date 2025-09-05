0053cfba11c42e0de9797d78a6f4dabfb439b7f7
NguyenDuongk5 <duongnvt2402@gmail.com>
2025-09-05 23:48:16 +0700

    gui file img

--- FE/flutter_application_1/lib/main.dart
+++ FE/flutter_application_1/lib/main.dart
@@ -10,9 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyHomePage(),
-    );
+    return const MaterialApp(home: MyHomePage());
   }
 }
 
